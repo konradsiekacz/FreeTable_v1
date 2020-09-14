c4f4a90aa6deeb48c20c4e2884f00b58d55bfd23
misohan <michal.hanusniak@aiesec.cz>
2020-09-14 19:43:00 +0200

    Change skeleton of project

--- src/main/java/com/mmkpdevelopers/freetable/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mmkpdevelopers.freetable.repository;
-
-import com.mmkpdevelopers.freetable.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Customer, Integer> {
-}
