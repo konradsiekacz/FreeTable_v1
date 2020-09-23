7603b9094519c6beb47c5e4ed5b43c0468bb58dc
konradsiekacz <konradsiekacz@gmail.com>
2020-09-23 14:53:43 +0200

    add http status in get method

--- 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/config/RestaurantTableController.java
+++ 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/config/RestaurantTableController.java
@@ -1,16 +1,13 @@
 package com.mmkpdevelopers.ecommerce.config;
 
-import com.mmkpdevelopers.ecommerce.dao.RestaurantTableRepository;
 import com.mmkpdevelopers.ecommerce.dto.RestaurantTableDto;
-import com.mmkpdevelopers.ecommerce.entity.RestaurantTable;
 import com.mmkpdevelopers.ecommerce.service.RestaurantTableService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -25,7 +22,7 @@ public class RestaurantTableController {
         this.restaurantTableService = restaurantTableService;
     }
 
-    @GetMapping("/tables")
+    @GetMapping(value = "/tables", produces = APPLICATION_JSON_VALUE)
     public List<RestaurantTableDto> getAllTables(){
         return restaurantTableService.getAllRestaurantTables().stream()
                 .map(table -> new RestaurantTableDto(table.getId(), table.getTableId(), table.getNumberInRestaurant(), table.getNumberInRestaurant())).collect(Collectors.toList());
