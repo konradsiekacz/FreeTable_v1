4d7c2f5bea045794903981b1a8c92f6125a72118
konradsiekacz <konradsiekacz@gmail.com>
2020-09-23 16:56:04 +0200

    refactor RestaurantTableDto

--- 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/dto/RestaurantTableDto.java
+++ 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/dto/RestaurantTableDto.java
@@ -1,10 +1,12 @@
 package com.mmkpdevelopers.ecommerce.dto;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Data
+@Getter
+@Setter
 public class RestaurantTableDto {
 
     private long tableId;
@@ -12,45 +14,10 @@ public class RestaurantTableDto {
     private long numberInRestaurant;
     private long numberOfSeats;
 
-    public RestaurantTableDto() {
-    }
-
-    public RestaurantTableDto(long id, long tableId,long numberInRestaurant, long numberOfSeats) {
+    public RestaurantTableDto(long id, long tableId, long numberInRestaurant, long numberOfSeats) {
         this.id = id;
         this.tableId = tableId;
         this.numberInRestaurant = numberInRestaurant;
         this.numberOfSeats = numberOfSeats;
-    }
-
-    public long getTableId() {
-        return tableId;
-    }
-
-    public void setTableId(long tableId) {
-        this.tableId = tableId;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    public void setNumberOfSeats(long numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
-
-    public long getNumberInRestaurant() {
-        return numberInRestaurant;
-    }
-
-    public void setNumberInRestaurant(long numberInRestaurant) {
-        this.numberInRestaurant = numberInRestaurant;
     }
 }
