080265d9790d0d6ffb7de7199bcdbcd3e7b926a1
konradsiekacz <konradsiekacz@gmail.com>
2020-09-23 13:14:31 +0200

    delete object RestaurantTable in controller

--- 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/config/RestaurantTableController.java
+++ 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/config/RestaurantTableController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class RestaurantTableController {
-    RestaurantTable restaurantTable = new RestaurantTable();
 
     @Autowired
     private RestaurantTableRepository restaurantTableRepository;
