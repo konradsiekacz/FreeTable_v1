41772f69a50a5a73177a6f0246b67078274860c4
marzako <martinnzakk@gmail.com>
2020-09-29 22:58:30 +0200

    Update CustomerController, CustomerService

--- 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/service/CustomerService.java
+++ 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/service/CustomerService.java
@@ -4,8 +4,11 @@ import com.mmkpdevelopers.ecommerce.dao.CustomerRepository;
 import com.mmkpdevelopers.ecommerce.entity.Customer;
 import com.mmkpdevelopers.ecommerce.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,11 +19,11 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
     public List<Customer> getCustomers () {
-        return customerRepository.findAll();
-//                .map(customer -> new Customer(customer.getId(), customer.getName(),
-//                        customer.getSurname(),
-//                        customer.getEmail(), customer.getPhoneNumber(), customer.getImageUrl()))
-//                .collect(Collectors.toList());
+        return customerRepository.findAll().stream()
+                .map(customer -> new Customer(customer.getId(), customer.getName(),
+                        customer.getSurname(),
+                        customer.getEmail(), customer.getPhoneNumber(), customer.getImageUrl()))
+                .collect(Collectors.toList());
     }
     public Customer getCustomerById (Long id) throws ResourceNotFoundException {
         return customerRepository.findAll().stream()
@@ -28,13 +31,20 @@ public class CustomerService {
                 .findAny()
                 .orElseThrow(() -> new ResourceNotFoundException("Customer with provided ID is not found"));
     }
-    public void addCustomer (Customer customer) {
-        customerRepository.save(customer);
+    public Customer addCustomer (Customer customer) {
+        return customerRepository.save(customer);
     }
     public void updateCustomer (Customer customer) {
         customerRepository.save(customer);
     }
-    public void deleteCustomer (Customer customer) {
+    public Map<String, Boolean> deleteCustomer(@PathVariable(value = "id") Long customerId)
+            throws org.springframework.data.rest.webmvc.ResourceNotFoundException {
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new org.springframework.data.rest.webmvc.ResourceNotFoundException("Customer not found for this id :: " + customerId));
+
         customerRepository.delete(customer);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
