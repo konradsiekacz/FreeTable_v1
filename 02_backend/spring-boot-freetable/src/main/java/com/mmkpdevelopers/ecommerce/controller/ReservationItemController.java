e54ddc48b5d74251e07473a64b5cf208c7b7afa3
konradsiekacz <konradsiekacz@gmail.com>
2020-10-02 10:57:57 +0200

    create ReservationItemController

--- 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/controller/ReservationItemController.java
+++ 02_backend/spring-boot-freetable/src/main/java/com/mmkpdevelopers/ecommerce/controller/ReservationItemController.java
@@ -1,7 +1,15 @@
 package com.mmkpdevelopers.ecommerce.controller;
 
+import com.mmkpdevelopers.ecommerce.converter.ReservationItemConverter;
+import com.mmkpdevelopers.ecommerce.converter.RestaurantConverter;
 import com.mmkpdevelopers.ecommerce.dao.ReservationItemRepository;
+import com.mmkpdevelopers.ecommerce.dto.ReservationItemDTO;
+import com.mmkpdevelopers.ecommerce.dto.RestaurantDTO;
 import com.mmkpdevelopers.ecommerce.entity.ReservationItem;
+import com.mmkpdevelopers.ecommerce.entity.Restaurant;
+import com.mmkpdevelopers.ecommerce.service.ReservationItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,22 +20,63 @@ import java.util.List;
 @RequestMapping("/api")
 public class ReservationItemController {
 
+    final ReservationItemService reservationItemService;
 
-    final ReservationItemRepository reservationItemRepository;
-
-    public ReservationItemController(ReservationItemRepository reservationItemRepository) {
-        this.reservationItemRepository = reservationItemRepository;
+    public ReservationItemController(ReservationItemService reservationItemService) {
+        this.reservationItemService = reservationItemService;
     }
 
-    @GetMapping("/reservationItems")
-    public List<ReservationItem> getAllReservationItems(){
-        return reservationItemRepository.findAll();
+    @GetMapping(value = "/reservationItems", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public List<ReservationItemDTO> getAllReservations() {
+        return ReservationItemConverter.entitiesToDto(reservationItemService.getAllReservationItems());
+    }
+
+    @GetMapping(value = "/reservationItems/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public ReservationItemDTO findReservationById(@PathVariable("id") Long id){
+        return ReservationItemConverter.entityToDto(reservationItemService.getReservationItemById(id));
     }
 
     @PostMapping("/reservationItems")
-    public List<ReservationItem> createReservationItems(@Valid @RequestBody List<ReservationItem> reservationItems) {
-        return reservationItemRepository.saveAll(reservationItems);
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addReservation(@RequestBody ReservationItemDTO reservationItemDTO){
+        reservationItemService.addReservationItem(ReservationItemConverter.dtoToEntity(reservationItemDTO));
     }
+
+    @PutMapping("/reservationItems")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public void updateReservation(@RequestBody ReservationItem reservationItem){
+        reservationItemService.updateReservationItem(reservationItem);
+    }
+
+    @DeleteMapping("/reservationItems/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteReservation(@PathVariable("id")long id){
+        reservationItemService.deleteReservationItem(id);
+    }
+
+    //    final ReservationItemRepository reservationItemRepository;
+//
+//    public ReservationItemController(ReservationItemRepository reservationItemRepository) {
+//        this.reservationItemRepository = reservationItemRepository;
+//    }
+
+
+//    @GetMapping("/reservationItems")
+//    public List<ReservationItem> getAllReservationItems(){
+//        return reservationItemRepository.findAll();
+//    }
+//
+//    @PostMapping("/reservationItems")
+//    public List<ReservationItem> createReservationItems(@Valid @RequestBody List<ReservationItem> reservationItems) {
+//        return reservationItemRepository.saveAll(reservationItems);
+//    }
 
 
 
